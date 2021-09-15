27d723cf43a4eae9d1d1a51409b68fd76b46e3c3
Trivita Annisa Utami <trivita1900018074@webmail.uad.ac.id>
2021-09-15 23:01:07 +0700

    sound update

--- Assets/Scripts/CharacterSoundController.cs
+++ Assets/Scripts/CharacterSoundController.cs
@@ -7,7 +7,6 @@ public class CharacterSoundController : MonoBehaviour
     public AudioClip jump;
     private AudioSource audioPlayer;
     public AudioClip scoreHighlight;
-    
 
     public void PlayScoreHighlight()
     {
